5f732c4234c7e19599b45abcb8ed59846129253d
hurryzyn <harry.muzayin@gmail.com>
2024-10-15 23:29:56 +0700

    view_edit

---
+++ resources/views/layout/footer.blade.php
@@ -0,0 +1,17 @@
+<div class="fot container-fluid py-5  ">
+    <footer class="row row-cols-1 row-cols-sm-2 row-cols-md-5  ">
+        <div class="col mb-3"> 
+            <p class="text-body-secondary">&copy; <i>Kas!rcafetaria</i></p>
+        </div>
+
+        <div class="col mb-3"></div>
+        <div class="col mb-3"></div>
+        <div class="col mb-3"></div>
+
+        <div class="col mb-3">
+            <a href="https://www.instagram.com/sistem_informasi10/"> 
+                <i class="fab fa-instagram fa-2x"></i>
+            </a>
+        </div>
+    </footer>
+</div>
